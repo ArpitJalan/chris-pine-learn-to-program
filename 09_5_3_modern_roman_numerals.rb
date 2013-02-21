847dbc23ba3e31ff5f619369c8abf7832c0f2923
Alberto De Lucca <albertodelucca@gmail.com>
2013-02-20 21:36:59 -0430

    added test conditions for modern romals

--- 09_5_3_modern_roman_numerals.rb
+++ 09_5_3_modern_roman_numerals.rb
@@ -12,44 +12,85 @@
 
 def modern_roman_numeral num
   
+  num_stored = num
   numeral = String.new
 
-  #how many thousands?
+# THOUSANDS
+
   thousands = num / 1000
   if thousands != 0 then numeral << "M" * thousands end
   num = num % 1000
 
-  # how many five_hundreds?
+# HUNDREDS
+
   five_hundreds = num / 500
-  if five_hundreds != 0 then numeral << "D" * five_hundreds end 
   num = num % 500
  
-  # how many hundreds?
   hundreds = num / 100
-  if hundreds != 0 then numeral << "C" * hundreds end
   num = num % 100
+
+  if five_hundreds == 1 && hundreds == 4
+
+    numeral << "CM"
+    
+  elsif five_hundreds == 0 && hundreds == 4
+    
+    numeral << "CD"
+
+  else
+
+    numeral << "D" * five_hundreds
+    numeral << "C" * hundreds
+    
+  end
   
-  # how many fifties?
+# TENS
+
   fifties = num / 50
-  if fifties != 0 then numeral << "L" * fifties end
   num = num % 50
 
-  # how many tens?
   tens = num / 10
-  if tens != 0 then numeral << "X" * tens end
   num = num % 10
 
-  # how many fives?
+  if fifties == 1 && tens == 4
+
+    numeral << "XC"
+    
+  elsif fifties == 0 && tens == 4
+    
+    numeral << "XL"
+
+  else
+
+    numeral << "L" * fifties
+    numeral << "X" * tens
+    
+  end
+
+# UNITS
+
   fives = num / 5
-  if fives != 0 then numeral << "V" * fives end
   num = num % 5
 
-  # how many units?
   ones = num / 1
-  if ones != 0 then numeral << "I" * ones end
   num = num % 1
 
-  puts "#{num} is #{numeral} in modern style Roman."
+    if fives == 1 && ones == 4
+
+    numeral << "IX"
+    
+  elsif fifties == 0 && tens == 4
+    
+    numeral << "IV"
+
+  else
+
+    numeral << "V" * fives
+    numeral << "I" * ones
+    
+  end
+
+  puts "#{num_stored} is #{numeral} in modern style Roman."
 end
 
-modern_roman_numeral 3999
+modern_roman_numeral 3219
